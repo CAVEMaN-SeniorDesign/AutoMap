2fccc543ebd38249ec67a0c348f7ccfdc8166df1
Kushal Parekh <kup9@pitt.edu>
2025-03-20 13:10:51 -0400

    new test folders, updated blender script, and filtering script for blurry images

--- blender_glb_output.py
+++ blender_glb_output.py
@@ -4,6 +4,10 @@ import sys
 # import glob # for arbitrary file finding w/ any extension
 import bpy
 
+
+list_of_usable_textures = [".exr", ".jpg", ".jpeg", ".tga", ".dds", ".psd"]
+texture_found = False
+
 n = len(sys.argv)
 print(n)
 
@@ -23,6 +27,9 @@ print(output_path)
 
 if(not os.path.isdir(input_path) or not os.path.isdir(output_path)):
     exit()
+
+input_path = input_path.replace("\\", "/")
+output_path = output_path.replace("\\", "/")
     
 if input_path[-1] == "/":
     input_path = input_path[:-1]
@@ -30,11 +37,25 @@ if input_path[-1] == "/":
 if output_path[-1] == "/":
     output_path = output_path[:-1]
 
+for filename in os.listdir(input_path):
+    name, ext = os.path.splitext(filename)
+    if ext:
+        if ext.lower() in list_of_usable_textures:
+            print("Texture file found")
+            texture_found = True
+            texture = filename
+
+if not texture_found:
+    print("Texture file not found")
+    exit()
+
 input_obj = input_path + "/texturedMesh.obj"
 input_mtl = input_path + "/texturedMesh.mtl"
-input_exr = input_path + "/texture_1001.exr"
+input_texture_file = input_path + "/" + texture
 output_glb = output_path + "/texturedMesh.glb"
 
+
+print(input_texture_file)
 
 #bpy.ops.wm.obj_import(filepath="C:/Users/parek/Desktop/SeniorDesign/RTABMAP/437desksetupdata/imageOuts/rgb/outputs/texturedMesh.obj", directory="C:/Users/parek/Desktop/SeniorDesign/RTABMAP/437desksetupdata/imageOuts/rgb/outputs")
 bpy.ops.object.select_all(action="SELECT")
---
+++ blurry_image_filter.py
@@ -0,0 +1,78 @@
+import os
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+
+
+# image_test = "C:/Users/parek/Downloads/4th_Floor_Hallway_Measuring_Test1/4th_Floor_Hallway_Measuring_Test1/Photos_Color_1742071924428.31933593750000.png"
+# image_test = "C:/Users/parek/Downloads/4th_Floor_Hallway_Measuring_Test1/4th_Floor_Hallway_Measuring_Test1/Photos_Color_1742071923706.56591796875000.png"
+# image_test = "C:/Users/parek/Downloads/4th_Floor_Hallway_Measuring_Test1/4th_Floor_Hallway_Measuring_Test1/Photos_Color_1742071924862.66601562500000.png"
+
+path = "C:/Users/parek/Downloads/4th_Floor_Hallway_Measuring_Test1/4th_Floor_Hallway_Measuring_Test1"
+# path = "C:/Users/parek/Desktop/SeniorDesign/RTABMAP/437desksetupdata/imageOuts/rgb"
+
+isRGB = False
+
+def laplace_variance(image):
+    return cv2.Laplacian(image, cv2.CV_64F).var()
+
+def RL_deconvolution(observed, psf, iterations):
+    # initial estimate is arbitrary - uniform 50% grey works fine
+    latent_est = 0.5*np.ones(observed.shape)
+    # create an inverse psf
+    psf_hat = psf[::-1,::-1]
+    # iterate towards ML estimate for the latent image
+    for i in np.arange(iterations):
+        est_conv      = cv2.filter2D(latent_est,-1,psf)
+        relative_blur = observed/est_conv
+        error_est     = cv2.filter2D(relative_blur,-1,psf_hat)
+        latent_est    = latent_est * error_est
+    return latent_est
+
+var_store = np.zeros(len(os.listdir(path)))
+
+# for file in os.listdir(path):
+    # os.rename(path + "/" + file, path + "/" + file.replace(".", ""))
+    # os.rename(path + "/" + file, path + "/" + file.replace("png", ".png"))
+
+
+
+# print(laplace_variance(cv2.imread("C:/Users/parek/Desktop/SeniorDesign/RTABMAP/437desksetupdata/imageOuts/rgb/1742162471608267.png")))
+
+for index, file in enumerate(os.listdir(path)):
+    # print(file)
+    if(os.path.isdir(path + "/" + file)):
+        continue
+    image = cv2.imread(path + "/" + file)
+    try:
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        isRGB = True
+    except:
+        gray = image
+    
+    # print(gray.dtype)
+    blurmetric = laplace_variance(gray)
+    # print(blurmetric)
+    # if blurmetric > 200:
+        # plt.figure()
+        # if isRGB:
+        #     plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+        # else:
+        #     plt.imshow(gray)
+        # print(file)
+    var_store[index] = blurmetric
+
+print(np.median(var_store))
+print(np.average(var_store))
+
+plt.figure()
+plt.hist(var_store, bins=50, color='red')
+plt.xlabel("Value")
+plt.ylabel("Freq")
+
+plt.show()
+
+# for image in list_images:
+#     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+#     blurmetric = laplace_variance(gray)
+
